c91dee432562cc1d0f5fa2543040add48099567a
tuananh <tuananh14111998@gmail.com>
2022-05-23 09:28:35 +0700

    Fix error for test_proper_boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     logger.info("Converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Remove outlier
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save the cleaned dataset
     logger.info("Saving the output artifact")
     file_name = "clean_sample.csv"
